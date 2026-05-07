67bfab57a7c201036367c340ff7190fc1cc6e3eb
Tino <xcaution255@gmail.com>
2026-05-07 10:38:27 +0300

    commited the project at the first time

---
+++ src/test/kotlin/com/devops/app/AppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devops.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
