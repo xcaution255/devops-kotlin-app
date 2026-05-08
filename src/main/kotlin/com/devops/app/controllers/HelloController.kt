b1d5f61e7e7305adc459b2cbb6e018a2678f693c
Tino <xcaution255@gmail.com>
2026-05-08 12:16:04 +0300

    added request mapping

--- src/main/kotlin/com/devops/app/controllers/HelloController.kt
+++ src/main/kotlin/com/devops/app/controllers/HelloController.kt
@@ -1,10 +1,11 @@
 package com.devops.app.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-//@RequestMapping("/")
+@RequestMapping("/api")
 class HelloController {
     @GetMapping("/hello")
     fun hello(): String {
