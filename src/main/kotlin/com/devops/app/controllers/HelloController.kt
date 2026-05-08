b35c9ea09f632177f784f312a1dad3874ac9dd94
Tino <xcaution255@gmail.com>
2026-05-08 12:07:54 +0300

    value on endpoint(users) changed

--- src/main/kotlin/com/devops/app/controllers/HelloController.kt
+++ src/main/kotlin/com/devops/app/controllers/HelloController.kt
@@ -12,14 +12,7 @@ class HelloController {
     }
 
     @GetMapping("/users")
-    fun getUsers(): Map<String, String> {
-         val users = mapOf<String, String>(
-            "1" to "AUGUSTINO",
-            "2" to "EMMANUEL",
-            "3" to "WINFRIDA",
-            "4" to "PASKALIA",
-            "5" to "ANNASTAZIA"
-        )
-     return users
+    fun getUsers(): String {
+        return "User : Augustino"
     }
 }
