d5a51ef038c8a52ea49bf87e2104574f77ccbd37
Tino <xcaution255@gmail.com>
2026-05-08 12:01:01 +0300

    new api endpoint added (users)

--- src/main/kotlin/com/devops/app/controllers/HelloController.kt
+++ src/main/kotlin/com/devops/app/controllers/HelloController.kt
@@ -10,4 +10,16 @@ class HelloController {
     fun hello(): String {
         return "Hello Devops!!"
     }
+
+    @GetMapping("/users")
+    fun getUsers(): Map<String, String> {
+         val users = mapOf<String, String>(
+            "1" to "AUGUSTINO",
+            "2" to "EMMANUEL",
+            "3" to "WINFRIDA",
+            "4" to "PASKALIA",
+            "5" to "ANNASTAZIA"
+        )
+     return users
+    }
 }
