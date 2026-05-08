c39991dc0c3ebdbd69487294b65a4b82f4429bbf
Tino <xcaution255@gmail.com>
2026-05-08 12:49:15 +0300

    moniroting app with prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    //for metrics
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 kotlin {
